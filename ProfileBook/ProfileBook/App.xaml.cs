1a51a864b809df816c276352d4c86f1f776a9491
Evgeniy Seletkov <evgeniy.seletkov@gmail.com>
2021-03-03 08:09:51 +0200

    Added project source.

---
+++ ProfileBook/ProfileBook/App.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using ProfileBook.Services.Profile;
+using ProfileBook.Services.Repository;
+using ProfileBook.ViewModels;
+using ProfileBook.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProfileBook
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        #region --- Overrides ---
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //Services
+            containerRegistry.RegisterInstance<IRepository>(Container.Resolve<Repository>());
+            containerRegistry.RegisterInstance<IProfileService>(Container.Resolve<ProfileService>());
+
+            //Navigation
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<SignInPage, SignInPageViewModel>();
+            containerRegistry.RegisterForNavigation<MainListPage, MainListPageViewModel>();
+            containerRegistry.RegisterForNavigation<SignUpPage, SignUpPageViewModel>();
+            containerRegistry.RegisterForNavigation<AddProfilePage, AddProfilePageViewModel>();
+        }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync($"{nameof(NavigationPage)}/{nameof(SignInPage)}");
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+
+        #endregion
+    }
+}
---
+++ ProfileBook/ProfileBook/ViewModels/SignInPageViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Prism.Mvvm;
+using Prism.Navigation;
+using ProfileBook.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ProfileBook.ViewModels
+{
+    class SignInPageViewModel : BindableBase
+    {
+        INavigationService navigationService;
+
+        private string title;
+
+        public string Title
+        {
+            get => title;
+            set => SetProperty(ref title, value);
+        }
+
+
+        public SignInPageViewModel(INavigationService navigationService)
+        {
+            Title = "Users SignIn";
+            this.navigationService = navigationService;
+        }
+
+        public ICommand SignInTapCommand => new Command(OnSignInTap);
+        public ICommand SignUpTapCommand => new Command(OnSignUpTap);
+
+        private async void OnSignInTap()
+        {
+            await navigationService.NavigateAsync($"/{nameof(NavigationPage)}/{nameof(MainListPage)}");
+        }
+
+        private async void OnSignUpTap()
+        {
+            await navigationService.NavigateAsync($"{nameof(SignUpPage)}");
+        }
+    }
+}
